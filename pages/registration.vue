b5e4fa2d05bd535eaff1203f2dc6d36d76c27724
Riad Kanane <one@kanane.com>
2020-01-30 12:51:39 +0000

    final update

---
+++ pages/registration.vue
@@ -0,0 +1,11 @@
+<template>
+  <registrationForm />
+</template>
+<script>
+import registrationForm from "../components/registration_form.vue";
+export default {
+  components: {
+    registrationForm
+  }
+};
+</script>
